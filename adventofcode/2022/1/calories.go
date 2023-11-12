7595b8f6fe96dcee2b39bc5b0710537a2db8e1b3
Julien Hallé <julienhalle@gmail.com>
2023-11-12 06:47:14 -0500

    advent of code 2022 day one part 2

--- adventofcode/2022/1/calories.go
+++ adventofcode/2022/1/calories.go
@@ -3,39 +3,44 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"strconv"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	input := []string{}
-	var res int
-	for  scanner.Scan() {
-		item := scanner.Text()
-		input = append(input, item)
+	items := []string{}
+	for scanner.Scan() {
+		items = append(items, scanner.Text())
 	}
 
-	res = calories(input)
+	res := calories(items, 3)
 
 	fmt.Println(res)
 }
 
-func calories(input []string) int {
-	var max = 0
+func calories(items []string, top int) int {
+	sums := []int{}
 	var sum = 0
 
-	for _, item := range input {
+	for _, item := range items {
 		cal, err := strconv.Atoi(item)
-		if(err != nil) {
+		if err != nil {
+			sums = append(sums, sum)
 			sum = 0
 		} else {
 			sum += cal
-			if(max < sum) {
-				max = sum
-			}
 		}
 	}
+	sums = append(sums, sum)
 
-	return max
+	sort.Ints(sums)
+
+	var res = 0
+	for _, item := range sums[len(sums)-top:] {
+		res += item
+	}
+
+	return res
 }
--- adventofcode/2022/1/calories_test.go
+++ adventofcode/2022/1/calories_test.go
@@ -2,8 +2,8 @@ package main
 
 import "testing"
 
-func PerformTest(t *testing.T, input []string, expect int) {
-	res := calories(input)
+func PerformTest(t *testing.T, input []string, top, expect int) {
+	res := calories(input, top)
 
 	if res != expect {
 		t.Errorf("from %v, got %d, wanted %d", input, res, expect)
@@ -14,19 +14,26 @@ func TestSimple(t *testing.T) {
 	input := []string{"1", "", "1", "1"}
 	expect := 2
 
-	PerformTest(t, input, expect)
+	PerformTest(t, input, 1, expect)
 }
 
 func TestEmpty(t *testing.T) {
 	input := []string{}
 	expect := 0
 
-	PerformTest(t, input, expect)
+	PerformTest(t, input, 1, expect)
 }
 
 func TestWithZeroValues(t *testing.T) {
 	input := []string{"1", "", "1", "0", "1"}
 	expect := 2
 
-	PerformTest(t, input, expect)
+	PerformTest(t, input, 1, expect)
+}
+
+func TestTop3(t *testing.T) {
+	input := []string{"3", "", "1", "", "1", "", "5", "", "4"}
+	expect := 12
+
+	PerformTest(t, input, 3, expect)
 }
