254b5ceb2a684ac4788887780d23da0db0b2a571
Julien Jouvent-Hallé <158200+jouvent@users.noreply.github.com>
2024-05-11 07:43:21 -0400

    Go tutorial: Return and handle error

--- tuto/greetings/greetings.go
+++ tuto/greetings/greetings.go
@@ -1,8 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v!", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// If a message was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Wellcome!", name)
+	return message, nil
 }
